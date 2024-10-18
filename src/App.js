81e5b790a4ef87af9030c5aefdb61fa86eda2eae
chendechuan <chen0802@qq.com>
2024-10-18 16:09:12 +0800

    1. 用 ref 来实现它,不会重新渲染组件；

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import { useState } from "react";
+import { useRef } from "react";
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
+  let countRef = useRef(0);
 
   function handleClick() {
-    setCount(count + 1);
+    // 这样并未重新渲染组件！
+    countRef.current = countRef.current + 1;
   }
 
-  return <button onClick={handleClick}>你点击了 {count} 次</button>;
+  return <button onClick={handleClick}>你点击了 {countRef.current} 次</button>;
 }
