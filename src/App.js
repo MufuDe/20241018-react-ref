351fb1d0fd964555917e0cc6e4e7d65f2a581d10
chendechuan <chen0802@qq.com>
2024-10-18 15:50:44 +0800

    1. 制作秒表;

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import { useRef } from "react";
+import { useState } from "react";
 
-export default function Counter() {
-  let ref = useRef(0);
+export default function Stopwatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
 
-  function handleClick() {
-    ref.current = ref.current + 1;
-    alert("你点击了 " + ref.current + " 次！");
+  function handleStart() {
+    // 开始计时。
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    setInterval(() => {
+      // 每 10ms 更新一次当前时间。
+      setNow(Date.now());
+    }, 10);
   }
 
-  return <button onClick={handleClick}>点击我！</button>;
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
+  return (
+    <>
+      <h1>时间过去了： {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>开始</button>
+    </>
+  );
 }
