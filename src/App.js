8885eeee9cd5fa7b4b8868397a6174d4b3da8858
chendechuan <chen0802@qq.com>
2024-10-18 16:28:48 +0800

    1. 同时 使用一个 state 变量（用于渲染）和 一个 ref（在 timeout 时读取当前文本）;

--- src/App.js
+++ src/App.js
@@ -2,16 +2,22 @@ import { useState, useRef } from "react";
 
 export default function Chat() {
   const [text, setText] = useState("");
+  const textRef = useRef(text);
+
+  function handleChange(e) {
+    setText(e.target.value);
+    textRef.current = e.target.value;
+  }
 
   function handleSend() {
     setTimeout(() => {
-      alert("正在发送：" + text);
+      alert("正在发送：" + textRef.current);
     }, 3000);
   }
 
   return (
     <>
-      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <input value={text} onChange={handleChange} />
       <button onClick={handleSend}>发送</button>
     </>
   );
